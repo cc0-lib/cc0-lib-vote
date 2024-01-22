4a5daa2bfdfe12b2771b5faa6e6d2108a90afa4d
Nero One <info@nero1.net>
2024-01-22 09:59:55 +0800

    refactor: homepage and section components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,16 @@
+import SplitLetters from "@/components/anim/split-letters";
+import Container from "@/components/container";
+import Footer from "@/components/section/footer";
+import Header from "@/components/section/header";
+
 export default function Home() {
   return (
-    <main className="w-full flex min-h-screen bg-zinc-100 flex-col items-center justify-between px-10 py-8 uppercase font-medium font-mono">
-      <div className="fixed inset-0 rounded-lg border-2 border-zinc-300 m-3"></div>
+    <Container>
       <Header />
-      <span className="font-chakra uppercase font-bold text-8xl">VOTE!</span>
+      <span className="font-chakra text-8xl font-bold uppercase">
+        <SplitLetters text="VOTE" delay={0.25} />
+      </span>
       <Footer />
-    </main>
+    </Container>
   );
 }
-
-const Header = () => (
-  <div className="w-full grid grid-cols-3 items-center">
-    <div>CC0-LIB ZINE - Special Edition 2</div>
-    <div className="text-center">5D:17H:20M</div>
-    <ul className="flex flex-row justify-between items-center">
-      <li>round</li>
-      <li>vote</li>
-      <li>leaderboard</li>
-      <li>faq</li>
-    </ul>
-  </div>
-);
-
-const Footer = () => (
-  <div className="w-full flex flex-row items-center justify-between">
-    <div>cover art round 2 community voting</div>
-    <div>total voted: 5/10</div>
-  </div>
-);
